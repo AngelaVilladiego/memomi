cd3f0a4f466654ab6f28b84b411e0ce9071a67ea
AngelaVilladiego <villadia@sheridancollege.ca>
2024-01-27 13:19:01 -0500

    added tailwind

---
+++ MemomiWeb/src/components/Memo/Memo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Memo = () => {
+  return (
+    <div className="rounded-lg border-blue-400 border-2 margin-10 height-full aspect-[1.414/1] bg-orange-400"></div>
+  );
+};
+
+export default Memo;
