db41ceadd7feef6d76ecca87e4bd8b2426331813
Michelle Yu Collins <michelle.yu.collins@gmail.com>
2024-01-28 05:19:27 -0500

    Merged integrate-front-back and added some extra tweaks

--- MemomiWeb/src/components/SearchBar/SearchBar.js
+++ MemomiWeb/src/components/SearchBar/SearchBar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const SearchBar = () => {
   return( 
-        <div className="rounded-lg ml-10 mr-10 mt-2 mb-2 w-full flex-row content-between justify-between">
+        <div className="rounded-lg ml-10 mr-10 border-2 border-memoblue-400 w-full flex-row content-between justify-between">
             <input className="text-sans text-memoblue-400" type="text" placeholder="File for a memo.." />
             <img ref=".../public/Search.png"/>
         </div>
