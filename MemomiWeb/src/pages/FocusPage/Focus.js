2e2060c0d98c8ccd5743b605dc6ed98aa93ae959
Michelle Yu Collins <michelle.yu.collins@gmail.com>
2024-01-28 01:49:26 -0500

    making it work

--- MemomiWeb/src/pages/FocusPage/Focus.js
+++ MemomiWeb/src/pages/FocusPage/Focus.js
@@ -6,12 +6,11 @@ import Features from "../Features";
 import Contact from "../Contact";
 import NaviBar from "../../components/NaviBar/NaviBar";
 import Memo from "../../components/Memo/Memo";
-import LoginButton from "../../components/LoginButton/LoginButton"
 import SearchBar from "../../components/SearchBar/SearchBar"
 
 function Focus() {
   return (
-    <div className="h-svh flex flex-col items-center p-8">
+    <div className="bg-default h-svh flex flex-col items-center p-8">
       <Router>
         <NaviBar />
         <Routes>
@@ -22,54 +21,8 @@ function Focus() {
         </Routes>
       </Router>
     </div>
-    //     <div className="Focus">
-    //       <div className="Focus-Title">
-    //         <div>
-    //           <h2>Morning Team Meeting</h2>
-    //         </div>
-    //         <div className="date">
-    //           <h3>Date</h3>
-    //           <input
-    //             type="text"
-    //             class="date-info"
-    //             id="textBox"
-    //             placeholder="27/01/24"
-    //           />
-    //         </div>
-    //       </div>
-
-    //       <div className="tags">
-    //         <ul>
-    //           <li className="attributed">Team</li>
-    //           <li className="attributed">Meeting</li>
-    //         </ul>
-    //       </div>
-
-    //       <div className="AI">
-    //         <button id="AIbutton" onClick={AIOn}>
-    //           Turn AI On
-    //         </button>
-    //         <div class="AI-Suggestions" id="AISuggestions">
-    //           <p>test</p>
-    //         </div>
-    //       </div>
-
-    //       <input
-    //         type="text"
-    //         class="content"
-    //         id="textBox"
-    //         placeholder="Type something..."
-    //       />
-    //     </div>
   );
 }
 
-function AIOn() {
-  var AItags = document.getElementById("AISuggestions");
-  AItags.style.display =
-    AItags.style.display === "none" || AItags.style.display === ""
-      ? "block"
-      : "none";
-}
 
 export default Focus;
--- MemomiWeb/tailwind.config.js
+++ MemomiWeb/tailwind.config.js
@@ -1,12 +1,32 @@
+const { scryRenderedComponentsWithType } = require('react-dom/test-utils');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        memoblue: {
+          50: "#D9D9D9",
+          400: "#5069B1",
+        },
+        memoneutral: {
+          800: "#333333",
+        },
+        memoyellow: {
+          50: "#FBF4E4",
+          100: "#f2ecdc",
+        },
+      },
+    },
     fontFamily: {
       sans: ["Poppins", "sans-serif"],
-      handwriting: ["Rock Salt", "cursive"],
+      fancy: ["Rock Salt", "cursive"],
+      handwriting: ["Poor Story", "system-ui"],
+    },
+    backgroundImage:{
+      'default': "src('./public/Background.png')"
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/line-clamp")],
 };
